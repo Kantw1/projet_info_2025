27efef3fd01588b3887265fdd661a7244b7d46e1
capu-ldr <capucine.lardiere03@gmail.com>
2025-04-13 23:46:13 +0200

    Update set_programmation_volet.php

--- PHP_request/set_programmation_volet.php
+++ PHP_request/set_programmation_volet.php
@@ -46,4 +46,11 @@ try {
     echo json_encode(["success" => false, "error" => $e->getMessage()]);
 }
 
+ // Incrémenter les points
+            $updateStmt = $conn->prepare("UPDATE USERS SET point = point + 2 WHERE idUser = ?");
+            $updateStmt->bind_param("i", $id);
+            $updateStmt->execute();
+            $updateStmt->close();
+
+
 $conn->close();
